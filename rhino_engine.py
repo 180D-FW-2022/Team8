4e6ad47eed94f1c26d21aca75fed56314a2a5b4f
Xuan-Huong Nguyen <xuan-huongnguyen@Xuan-Huongs-MacBook-Pro.local>
2022-11-08 19:20:14 -0800

    set up rhino speech engine

---
+++ rhino_engine.py
@@ -0,0 +1,7 @@
+import pvrhino
+
+
+rhino = pvrhino.create(
+   access_key='${4n7j8/reOKePM5xXFp+CmSFnBsgRZ5EF2m9bjghxif/OpZCG/LHcnw==}',
+   context_path='${/Users/xuan-huongnguyen/Desktop/Team8/demo.rhn}'
+)
