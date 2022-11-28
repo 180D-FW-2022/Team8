e190ccf95b07ec01c1815288917f30691f8fa3c9
Xuan-Huong Nguyen <xuan-huongnguyen@Xuan-Huongs-MacBook-Pro.local>
2022-11-28 00:30:40 -0800

    run python3 spotify.py and follow the URL. this will direct you to a gui to interact with the controls of spotify

---
+++ spotify.py
@@ -0,0 +1,187 @@
+# import json
+# import spotipy
+# import os
+# import webbrowser
+# import numpy as np
+
+# username = 'erica.nguyen32'
+# clientID = 'ae600be297a948a49eddea1447e72d5e'
+# clientSecret = '2f638f0f5264430f842a0b8d95e0123a'
+# redirect_uri = 'http://localhost/'
+# oauth_object = spotipy.SpotifyOAuth(clientID, clientSecret, redirect_uri)
+# token_dict = oauth_object.get_access_token()
+# token = token_dict['access_token']
+# spotifyObject = spotipy.Spotify(auth=token)
+# user_name = spotifyObject.current_user()
+
+"""
+Prerequisites
+
+    pip3 install spotipy Flask Flask-Session
+
+    // from your [app settings](https://developer.spotify.com/dashboard/applications)
+    export SPOTIPY_CLIENT_ID=client_id_here
+    export SPOTIPY_CLIENT_SECRET=client_secret_here
+    export SPOTIPY_REDIRECT_URI='http://127.0.0.1:8080' // must contain a port
+    // SPOTIPY_REDIRECT_URI must be added to your [app settings](https://developer.spotify.com/dashboard/applications)
+    OPTIONAL
+    // in development environment for debug output
+    export FLASK_ENV=development
+    // so that you can invoke the app outside of the file's directory include
+    export FLASK_APP=/path/to/spotipy/examples/app.py
+ 
+    // on Windows, use `SET` instead of `export`
+
+Run app.py
+
+    python3 app.py OR python3 -m flask run
+    NOTE: If receiving "port already in use" error, try other ports: 5000, 8090, 8888, etc...
+        (will need to be updated in your Spotify app and SPOTIPY_REDIRECT_URI variable)
+"""
+
+import os
+from flask import Flask, session, request, redirect
+from flask_session import Session
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+import json
+import webbrowser
+import numpy as np
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(64)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = './.flask_session/'
+Session(app)
+
+SPOTIPY_CLIENT_ID='a5ba3a2595154188bc92d807932a7d1c'
+SPOTIPY_CLIENT_SECRET='6f6244fdcb304ac986d709ac1feedb7d'
+SPOTIPY_REDIRECT_URI='http://localhost:8080'
+scope = "user-read-playback-state,user-modify-playback-state"
+
+sp_oauth = SpotifyOAuth(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, scope=scope)
+
+@app.route('/')
+def index():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI, scope='user-read-currently-playing playlist-modify-private',
+                                               cache_handler=cache_handler,
+                                               show_dialog=True)
+
+    if request.args.get("code"):
+        # Step 2. Being redirected from Spotify auth page
+        auth_manager.get_access_token(request.args.get("code"))
+        return redirect('/')
+
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        # Step 1. Display sign in link when no token
+        auth_url = auth_manager.get_authorize_url()
+        return f'<h2><a href="{auth_url}">Sign in</a></h2>'
+
+    # Step 3. Signed in, display data
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return f'<h2>Hi {spotify.me()["display_name"]}, ' \
+           f'<small><a href="/sign_out">[sign out]<a/></small></h2>' \
+           f'<a href="/playlists">my playlists</a> | ' \
+           f'<a href="/currently_playing">currently playing</a> | ' \
+           f'<a href="/start_playback">start playback</a> | ' \
+        f'<a href="/current_user">me</a>' \
+
+
+
+@app.route('/sign_out')
+def sign_out():
+    session.pop("token_info", None)
+    return redirect('/')
+
+
+@app.route('/playlists')
+def playlists():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        return redirect('/')
+
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return spotify.current_user_playlists()
+
+
+@app.route('/currently_playing')
+def currently_playing():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        return redirect('/')
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    track = spotify.current_user_playing_track()
+    if not track is None:
+        return track
+    return "No track currently playing."
+
+@app.route('/start_playback')
+def start_playback():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        return redirect('/')
+    spotify = spotipy.Spotify(auth_manager=sp_oauth)
+    return spotify.start_playback(device_id='f18bf9d465b761b38a3af2f296db8fcb354de94d')
+
+	
+
+@app.route('/current_user')
+def current_user():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(cache_handler=cache_handler)
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        return redirect('/')
+    spotify = spotipy.Spotify(auth_manager=auth_manager)
+    return spotify.current_user()
+
+
+'''
+Following lines allow application to be run more conveniently with
+`python app.py` (Make sure you're using python3)
+(Also includes directive to leverage pythons threading capacity.)
+'''
+if __name__ == '__main__':
+    app.run(threaded=True, port=int(os.environ.get("PORT",
+                                                   os.environ.get("SPOTIPY_REDIRECT_URI", 'http://localhost:8080').split(":")[-1])))
+
+#device id array
+
+device_id = np.array( [ ['f18bf9d465b761b38a3af2f296db8fcb354de94d'],['96dd25fddb7899ed0c54eca8157199ceaf670d1d'], ['9ccb1139563c330086c0758871e1d7210201b0ec'] ] )
+
+
+# To print the JSON response from
+# browser in a readable format.
+# optional can be removed
+# print(json.dumps(user_name, sort_keys=True, indent=4))
+
+# while True:
+# 	print("Welcome to the project, " + user_name['display_name'])
+# 	print("1 - Web Browser")
+# 	print("2 - Spotify Browser")
+# 	user_input = int(input("Select Your Device ID: "))
+# 	if user_input == 1:	device_ID = device_id[0][0]
+# 	elif user_input == 2: device_ID = device_id[1][0]
+# 	elif user_input == 3: device_ID = device_id[2][0]
+# 	print("0 - Exit the console")
+# 	print("1 - Search for a Song")
+# 	user_input = int(input("Enter Your Choice: "))
+# 	if user_input == 1:
+# 		#search_song = input("Enter the song name: ")
+# 		results = spotifyObject.start_playback(device_id=device_ID)#, context_uri="spotify:artist:5cj0lLjcoR7YOSnhnX0Po5")
+# 		#results = spotifyObject.search(search_song, 1, 0, "track")
+# 		#songs_dict = results['tracks']
+# 		#song_items = songs_dict['items']
+# 		#song = song_items[0]['external_urls']['spotify']
+# 		#webbrowser.open(song)
+# 		#print('Song has opened in your browser.')
+# 	elif user_input == 0:
+# 		print("Good Bye, Have a great day!")
+# 		break
+# 	else:
+# 		print("Please enter valid user-input.")
