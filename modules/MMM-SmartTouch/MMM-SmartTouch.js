f1114ddb19ffee88641019fb86f5ecbafe161967
davidpaek09 <114616347+davidpaek09@users.noreply.github.com>
2023-03-15 12:48:35 -0700

    Add files via upload

---
+++ modules/MMM-SmartTouch/MMM-SmartTouch.js
@@ -0,0 +1,198 @@
+/* Magic Mirror
+ * MMM-SmartTouch.js
+ *
+ * By SmartBuilds.io - Pratik and Eben
+ * https://smartbuilds.io
+ * MIT Licensed.
+ */
+
+Module.register("MMM-SmartTouch", {
+  defaults: {
+  },
+
+  start: function () {
+    Log.info(this.name + " has started...");
+    this.sendSocketNotification("CONFIG", this.config);
+  },
+
+  getStyles: function () {
+    return [this.file("css/mmm-smarttouch.css"), "font-awesome.css"];
+  },
+
+  // Load translations files
+  getTranslations: function () {
+    return {
+      en: "translations/en.json",
+      nb: "translations/nb.json",
+    };
+  },
+
+  createContainerDiv: function () {
+    const containerDiv = document.createElement("div");
+    containerDiv.className = "st-container";
+
+    return containerDiv;
+  },
+
+  toggleStandby: function () {
+    const existingBodyClass = document.body.className;
+    if (existingBodyClass === "st-standby show") {
+      document.body.className = "st-standby fade";
+      console.log("toggle pressed");
+      //this.sendSocketNotification("test_not", null);
+    } else {
+      document.body.className = "st-standby show";
+    }
+  },
+
+  toggleONStandby: function () { //it is off and I want it on
+    const existingBodyClass = document.body.className;
+    if (existingBodyClass === "st-standby show") {
+      document.body.className = "st-standby fade";
+    } else {
+      document.body.className = "st-standby show";
+    }
+  },
+
+  toggleOFFStandby: function () { //it is on and I want it off
+    const existingBodyClass = document.body.className;
+    if (existingBodyClass === "st-standby show") {
+      document.body.className = "st-standby fade";
+    } 
+  },
+
+
+
+
+
+
+
+  createStandByButtonDiv: function () {
+    const standByButtonDiv = document.createElement("div");
+    standByButtonDiv.className = "st-container__standby-button";
+
+    standByButtonDiv.appendChild(document.createElement("span"))
+    standByButtonDiv.addEventListener("click", () => this.toggleStandby());
+
+    return standByButtonDiv;
+  },
+
+  toggleSideMenu: function () {
+    const menuToggleDiv = document.getElementById("st-menu-toggle")
+    menuToggleDiv.classList.toggle('show');
+
+    const mainMenuDiv = document.getElementById("st-main-menu")
+    mainMenuDiv.classList.toggle('show')
+  },
+
+  createMenuToggleButtonDiv: function () {
+    const menuToggleButtonDiv = document.createElement("div");
+    menuToggleButtonDiv.className = "st-container__menu-toggle";
+    menuToggleButtonDiv.id = "st-menu-toggle";
+
+    const hamburgerLineOne = document.createElement("div");
+    hamburgerLineOne.className = "st-container__menu-toggle st-toggle__bar_one";
+
+    const hamburgerLineTwo = document.createElement("div");
+    hamburgerLineTwo.className = "st-toggle__bar_two";
+
+    const hamburgerLineThree = document.createElement("div");
+    hamburgerLineThree.className = "st-toggle__bar_three";
+
+    menuToggleButtonDiv.appendChild(hamburgerLineOne);
+    menuToggleButtonDiv.appendChild(hamburgerLineTwo);
+    menuToggleButtonDiv.appendChild(hamburgerLineThree);
+
+    menuToggleButtonDiv.addEventListener("click", () => this.toggleSideMenu());
+
+    return menuToggleButtonDiv;
+  },
+
+  createShutdownButton: function () {
+    const shutdownButtonItem = document.createElement("li");
+    shutdownButtonItem.innerHTML = "<span class='fa fa-power-off fa-3x'></span>"
+        + "<br>" +  this.translate('SHUTDOWN');
+    shutdownButtonItem.className = "li-t"
+
+    // Send shutdown notification when clicked
+    shutdownButtonItem.addEventListener("click",
+        () => this.sendSocketNotification("SHUTDOWN", {}));
+
+    return shutdownButtonItem
+  },
+
+  createRestartButton: function () {
+    const restartButtonItem = document.createElement("li");
+    restartButtonItem.innerHTML = "<span class='fa fa-repeat fa-3x'></span>"
+        + "<br>" + this.translate('RESTART');
+    restartButtonItem.className = "li-t"
+
+    // Send restart notification when clicked
+    restartButtonItem.addEventListener("click",
+        () => this.sendSocketNotification("RESTART", {}));
+
+    return restartButtonItem
+  },
+
+  createMainMenuDiv: function () {
+    const mainMenuDiv = document.createElement("div");
+    mainMenuDiv.className = "st-container__main-menu";
+    mainMenuDiv.id = "st-main-menu";
+
+    const shutdownButton = this.createShutdownButton();
+    const restartButton = this.createRestartButton();
+
+    const buttonList = document.createElement("ul");
+    buttonList.appendChild(shutdownButton);
+    buttonList.appendChild(restartButton);
+
+    mainMenuDiv.appendChild(buttonList);
+
+    return mainMenuDiv;
+  },
+
+  getDom: function () {
+    // Initial standby state
+    document.body.className = "st-standby show";
+
+    const container = this.createContainerDiv();
+
+    const standByButton = this.createStandByButtonDiv();
+    container.appendChild(standByButton);
+
+    const menuToggleButton = this.createMenuToggleButtonDiv();
+    container.appendChild(menuToggleButton);
+
+    const mainMenu = this.createMainMenuDiv();
+    document.body.appendChild(mainMenu);
+
+    return container;
+  },
+
+
+  
+  notificationReceived: function (notification, payload, sender) {
+    if (notification === "TOGGLE") {
+      console.log("payload = ", payload);
+      //this.config.message = payload;
+      //this.toggleONStandby();
+      //() => this.toggleStandby();
+     // this.toggleStandby();
+      console.log("toggle received");
+      if (payload == 1) {
+        this.toggleONStandby();
+      }
+      if (payload == 0) {
+        this.toggleOFFStandby();
+      }
+    }
+    
+  },
+
+
+
+  // Recieve notification from sockets via nodehelper.js
+  socketNotificationReceived: function (notification, payload) {
+  },
+
+});
